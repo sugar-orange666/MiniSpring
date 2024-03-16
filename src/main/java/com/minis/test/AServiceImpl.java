393e595ec4c28d692c49ae5931380b2e9d92ccf8
songwenjinger <13201656250@163.com>
2024-03-16 13:08:55 +0800

    xml引入属性，但是这时候还没有注入，属性的值是null

--- src/main/java/com/minis/test/AServiceImpl.java
+++ src/main/java/com/minis/test/AServiceImpl.java
@@ -6,8 +6,40 @@ package com.minis.test;
  */
 public class AServiceImpl implements AService {
 
+
+    public AServiceImpl() {
+    }
+
+    private String property1;
+    private String property2;
+
+    private String name;
+    private int level;
+
+    public AServiceImpl(String name, int level) {
+        this.name = name;
+        this.level = level;
+        System.out.println(this.name + "," + this.level);
+    }
+
+    public void setProperty1(String property1) {
+        this.property1 = property1;
+    }
+
+    public String getProperty1() {
+        return property1;
+    }
+
+    public String getProperty2() {
+        return property2;
+    }
+
+    public void setProperty2(String property2) {
+        this.property2 = property2;
+    }
+
     @Override
     public void sayHello() {
-        System.out.println("a service 1 say Hello");
+        System.out.println(this.property1 + "," + this.property2);
     }
 }
