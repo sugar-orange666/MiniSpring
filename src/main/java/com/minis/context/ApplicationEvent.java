d68406ad5f8778f19d801ccd702491895e5417e4
songwenjinger <13201656250@163.com>
2024-03-16 13:01:18 +0800

    新增监听和发布事件的类

---
+++ src/main/java/com/minis/context/ApplicationEvent.java
@@ -0,0 +1,15 @@
+package com.minis.context;
+
+import java.util.EventObject;
+
+//监听事件
+public class ApplicationEvent extends EventObject {
+
+	private static final long serialVersionUID = 1L;
+
+	public ApplicationEvent(Object arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
