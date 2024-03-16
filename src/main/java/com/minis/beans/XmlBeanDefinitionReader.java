12b400908bb151162595bb8e627e7dceab91dc6c
songwenjinger <13201656250@163.com>
2024-03-16 15:11:55 +0800

    把属性和构造方法的值放入内存中

--- src/main/java/com/minis/beans/XmlBeanDefinitionReader.java
+++ src/main/java/com/minis/beans/XmlBeanDefinitionReader.java
@@ -3,6 +3,8 @@ package com.minis.beans;
 import com.minis.core.Resource;
 import org.dom4j.Element;
 
+import java.util.List;
+
 /**
  * @Date：2024/3/16 10:08
  * @Description
@@ -22,7 +24,27 @@ public class XmlBeanDefinitionReader {
             String beanId = element.attributeValue("id");
             String beanClassName = element.attributeValue("class");
             BeanDefinition beanDefinition = new BeanDefinition(beanId, beanClassName);
-            simpleBeanFactory.registerBeanDefinition(beanId,beanDefinition);
+            simpleBeanFactory.registerBeanDefinition(beanId, beanDefinition);
+            //处理属性
+            List<Element> propertiesElements = element.elements("property");
+            PropertyValues pvs = new PropertyValues();
+            for (Element e : propertiesElements) {
+                String pType = e.attributeValue("type");
+                String pName = e.attributeValue("name");
+                String pValue = e.attributeValue("value");
+                pvs.addPropertyValue(pType, pName, pValue);
+            }
+            beanDefinition.setPropertyValues(pvs);
+            //处理构造方法
+            List<Element> constructorElements = element.elements("constructor-arg");
+            ArgumentValues avs = new ArgumentValues();
+            for (Element e : constructorElements) {
+                String aType = e.attributeValue("type");
+                String aName = e.attributeValue("name");
+                String aValue = e.attributeValue("value");
+                avs.addArgumentValue(new ArgumentValue(aType, aName, aValue));
+            }
+            beanDefinition.setConstructorArgumentValues(avs);
         }
 
     }
